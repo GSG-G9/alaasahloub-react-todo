e2161f5f88e22ad69d566bbcc625b0bc23958f6a
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2020-12-31 14:16:47 +0200

    Add function add task Relates #2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      newTask : [],
+    };
+  }
+
+
+  setNewTask = (event) =>{
+    event.preventDefault()
+    this.setState((previousState)=>{
+      return {
+        newTask: [...previousState.newTask, event.target[0].value]
+      }
+    });
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>ToDo List</h1>
+          <form onSubmit={this.setNewTask}>
+            <label>
+              New Task
+              <input type="text" name ="newTask" />
+              <input type="submit" value="add" />
+            </label>  
+            <div className="Tasks">
+              <ul>
+                  {
+                    this.state.newTask.map((task, index)=>{
+                      return <li key={index}>
+                        {task}
+                        </li>
+                    })
+                  }
+              </ul>
+            </div>
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
